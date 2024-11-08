246790145e48eb01b387764dfb622a7a49f608d1
Ryo23sanyu <dobokuka4@gmail.com>
2024-11-08 21:53:17 +0900

    add

--- intect/settings.py
+++ intect/settings.py
@@ -188,8 +188,7 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
     AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-    H31_BRIDGE_EXCEL = os.environ["H31_BRIDGE_EXCEL"]
-
+    
     # mediaファイル保存先
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # 保存先URL
